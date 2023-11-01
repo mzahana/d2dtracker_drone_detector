86cc5cd0216952bb528fe746db00b052278d0f8b
MohamedAbdelkader <mohamedashraf123@gmail.com>
2023-11-01 23:43:26 +0300

    Update test/test_gru_model.py

--- test/test_gru_model.py
+++ test/test_gru_model.py
@@ -26,7 +26,6 @@ class TrajectoryPredictor(nn.Module):
 
 # Velocity Predictor Model Definition
 class VelocityPredictor(nn.Module):
-    # Define the architecture of your velocity predictor model here
     def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
         super(VelocityPredictor, self).__init__()
 
@@ -54,40 +53,23 @@ def compute_velocity(position_sequence, dt):
     velocity = diff / dt
     return velocity
 
-def plot_trajectories_with_matplotlib(inputs, predictions):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot(inputs[:, 0], inputs[:, 1], inputs[:, 2], label='Input Sequence', color='blue', linewidth=2)
-    ax.plot(predictions[:, 0], predictions[:, 1], predictions[:, 2], label='Predicted Trajectory', color='red', linestyle='--', linewidth=2)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax.set_xlim(-10, 10)
-    ax.set_ylim(-10, 10)
-    ax.set_zlim(-10, 10)
-    plt.legend()
-    plt.show()
-
-def plot_velocity_in_3D(input_velocity, predicted_velocity):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+def plot_on_ax(ax, inputs, predictions, actuals=None, title='', linestyle='-', color='blue'):
+    ax.plot(inputs[:, 0], inputs[:, 1], inputs[:, 2], label='Input', linestyle=linestyle, color=color, linewidth=2)
+    ax.plot(predictions[:, 0], predictions[:, 1], predictions[:, 2], label='Predicted', linestyle='--', color='red', linewidth=2)
     
-    # Plotting the input 3D velocity sequence
-    ax.plot(input_velocity[:, 0], input_velocity[:, 1], input_velocity[:, 2], 
-            label='Input Velocity Sequence', color='blue', linewidth=2)
+    # Plot the actuals if provided
+    if actuals is not None:
+        ax.plot(actuals[:, 0], actuals[:, 1], actuals[:, 2], label='Actual', linestyle=':', color='green', linewidth=2)
     
-    # Plotting the predicted 3D velocity sequence
-    ax.plot(predicted_velocity[:, 0], predicted_velocity[:, 1], predicted_velocity[:, 2], 
-            label='Predicted Velocity Sequence', color='red', linestyle='--', linewidth=2)
+    ax.set_title(title)
     
-    ax.set_xlabel('Velocity in X')
-    ax.set_ylabel('Velocity in Y')
-    ax.set_zlabel('Velocity in Z')
-    ax.set_xlim(-10, 10)
-    ax.set_ylim(-10, 10)
-    ax.set_zlim(-10, 10)
-    plt.legend()
-    plt.show()
+    min_x, max_x = min(inputs[:, 0].min(), predictions[:, 0].min(), (actuals[:, 0].min() if actuals is not None else predictions[:, 0].min())), max(inputs[:, 0].max(), predictions[:, 0].max(), (actuals[:, 0].max() if actuals is not None else predictions[:, 0].max()))
+    min_y, max_y = min(inputs[:, 1].min(), predictions[:, 1].min(), (actuals[:, 1].min() if actuals is not None else predictions[:, 1].min())), max(inputs[:, 1].max(), predictions[:, 1].max(), (actuals[:, 1].max() if actuals is not None else predictions[:, 1].max()))
+    min_z, max_z = min(inputs[:, 2].min(), predictions[:, 2].min(), (actuals[:, 2].min() if actuals is not None else predictions[:, 2].min())), max(inputs[:, 2].max(), predictions[:, 2].max(), (actuals[:, 2].max() if actuals is not None else predictions[:, 2].max()))
+    
+    ax.set_xlim(min_x - 5, max_x + 5)
+    ax.set_ylim(min_y - 5, max_y + 5)
+    ax.set_zlim(min_z - 5, max_z + 5)
 
 def compute_predicted_positions(last_position, predicted_velocity, dt):
     predicted_positions = [last_position]
@@ -95,25 +77,6 @@ def compute_predicted_positions(last_position, predicted_velocity, dt):
         new_position = predicted_positions[-1] + v * dt
         predicted_positions.append(new_position)
     return np.array(predicted_positions)
-
-def plot_predicted_positions_using_velocity(input_sequence, predicted_positions_from_velocity):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    
-    ax.plot(input_sequence[:, 0], input_sequence[:, 1], input_sequence[:, 2], 
-            label='Input Position Sequence', color='blue', linewidth=2)
-    
-    ax.plot(predicted_positions_from_velocity[:, 0], predicted_positions_from_velocity[:, 1], predicted_positions_from_velocity[:, 2], 
-            label='Predicted Position from Velocity', color='red', linestyle='--', linewidth=2)
-    
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax.set_xlim(-10, 10)
-    ax.set_ylim(-10, 10)
-    ax.set_zlim(-10, 10)
-    plt.legend()
-    plt.show()
 
 if __name__ == '__main__':
     position_model_path = sys.argv[1]
@@ -140,16 +103,27 @@ if __name__ == '__main__':
     dt = 0.1
     speed = 7.0  # Speed along the trajectory in meters/second
     traj_type='line'
+    inp_seq_len = 20
+    out_seq_len = 10
     if traj_type=='line':
         # Define a straight line of 3D points
         velocity_x = speed  # constant velocity in the x-axis
-        timesteps = np.arange(20) * dt
+        velocity_y = speed
+        velocity_z = speed
+        timesteps = np.arange(inp_seq_len+1+out_seq_len) * dt
         
         # Start from an arbitrary point, for example (5, 3, 2)
-        start_position = np.array([0, 0, 0])
+        start_position = np.array([-10, 5, 3])
         x_coords = start_position[0] + velocity_x * timesteps
-        y_coords = np.full_like(timesteps, start_position[1])
-        z_coords = np.full_like(timesteps, start_position[2])
+        y_coords = start_position[1] + velocity_y * timesteps #np.full_like(timesteps, start_position[1])
+        z_coords = start_position[2] + velocity_z * timesteps #np.full_like(timesteps, start_position[2])
+
+        true_predicted_x=x_coords[-out_seq_len:]
+        true_predicted_y=y_coords[-out_seq_len:]
+        true_predicted_z=z_coords[-out_seq_len:]
+        x_coords = x_coords[:inp_seq_len+1]
+        y_coords = y_coords[:inp_seq_len+1]
+        z_coords = z_coords[:inp_seq_len+1]
 
     elif traj_type=='circle':
         ####################### Define the arc trajectory #######################
@@ -162,16 +136,25 @@ if __name__ == '__main__':
         theta_increment = distance_per_timestep / r
         
         # Generate angles for 20 timesteps
-        timesteps = np.arange(20) * theta_increment
+        timesteps = np.arange(inp_seq_len+1+out_seq_len) * theta_increment
         
         x_coords = xi + r * np.cos(timesteps)
         y_coords = yi + r * np.sin(timesteps)
         z_coords = np.full_like(timesteps, h)
+
+        true_predicted_x=x_coords[-out_seq_len:]
+        true_predicted_y=y_coords[-out_seq_len:]
+        true_predicted_z=z_coords[-out_seq_len:]
+        x_coords = x_coords[:inp_seq_len+1]
+        y_coords = y_coords[:inp_seq_len+1]
+        z_coords = z_coords[:inp_seq_len+1]
     else:
         print(f"traj_type {traj_type} is not valid")
         exit(1)
     
     sequence = np.vstack([x_coords, y_coords, z_coords]).T
+
+    true_pred_sequence = np.vstack([true_predicted_x, true_predicted_y, true_predicted_z]).T
 
     # Compute velocity from the 21-point input position sequence
     input_velocity = compute_velocity(sequence, dt=0.1)
@@ -181,19 +164,28 @@ if __name__ == '__main__':
 
     # Predict the output velocity sequence using the input velocity
     predicted_velocity_output = predict_trajectory(velocity_model, input_velocity)
-
-    # Print and plot results
-    print("Predicted Position Trajectory:")
-    print(predicted_position)
-
-    print("\nPredicted Velocity Output:")
-    print(predicted_velocity_output)
-
-    plot_trajectories_with_matplotlib(sequence[:-1], predicted_position)
-    plot_velocity_in_3D(input_velocity, predicted_velocity_output)
-
+    
     # Compute the predicted position using the last point in the input sequence, predicted velocities and dt
     predicted_positions_from_velocity = compute_predicted_positions(sequence[-1], predicted_velocity_output, dt)
 
-    # Plot the results
-    plot_predicted_positions_using_velocity(sequence, predicted_positions_from_velocity)
+    # Instead of separate figures, use one figure with 3 subplots
+    fig = plt.figure(figsize=(10, 15))
+    ax1 = fig.add_subplot(311, projection='3d')
+    ax2 = fig.add_subplot(312, projection='3d')
+    ax3 = fig.add_subplot(313, projection='3d')
+    
+    # Plot on the subplots
+    plot_on_ax(ax1, sequence[:-1], predicted_position, actuals=true_pred_sequence, title=traj_type+' Position Trajectory')
+    plot_on_ax(ax2, input_velocity, predicted_velocity_output, title='Velocity Trajectory')
+    plot_on_ax(ax3, sequence, predicted_positions_from_velocity, actuals=true_pred_sequence, title='Position from Velocity')
+
+    # Set global legend for the figure
+    handles, labels = ax1.get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper right')
+
+    # Adjust layout and show the plot
+    plt.tight_layout()
+    plt.show()
+
+    # Save the figure as a PDF
+    fig.savefig(traj_type+'_trajectory.pdf', format='pdf')
